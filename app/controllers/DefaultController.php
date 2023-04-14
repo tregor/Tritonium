9c4f28afef38ae9831d14dc83d76dfb1bb9835ca
tregor <tregor1997@gmail.com>
2023-04-14 13:10:36 -0700

    Defaults - DefaultController tests

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Tritonium\App\Controllers;
 
 use Tritonium\App\Models\ActiveUser;
+use Tritonium\Base\BaseEvent;
 use Tritonium\Base\Controllers\BaseController;
 use Tritonium\Base\Models\Migrations;
 use Tritonium\Base\Services\Console;
@@ -14,13 +15,27 @@ class DefaultController extends BaseController
 	public function actionIndex() {
 		App::$components->view->setCode(200)->setHeader('Content-Type', 'text/html;charset=UTF8')->render('default');
 	}
-	public function actionTesttest() {
-		App::$view->setCode(200)->setHeader('Content-Type', 'text/html;charset=UTF8')->render('default');
-	}
 	
-	public function actionTest() {
+	public function actionTestActiveModels() {
 		$users = ActiveUser::find()->where('token', 'IS', NULL)->all();
 		$users = ActiveUser::find()->all();
 		var_dump($users);
+	}
+	
+	public function actionTestEvents() {
+		$this->on('messageSend', function (BaseEvent $event) {
+			var_dump($event->getData());
+		});
+		$this->trigger('messageSend', 'test1');
+		
+		$this->on('messageSend', 'var_dump');
+		$this->trigger('messageSend', ['test2' => TRUE]);
+		
+		$this->on('messageSend', [$this, 'messageReceived']);
+		$this->trigger('messageSend', new \StdClass);
+	}
+	
+	public function messageReceived(){
+		echo('message received!');
 	}
 }
